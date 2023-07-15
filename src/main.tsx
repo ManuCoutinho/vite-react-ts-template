33cd578ddd7732b0007708cb98a598e8c2ec2074
ManuCoutinho <gdn.sasha@gmail.com>
2023-07-15 13:46:16 -0300

    Revert "chore: update absolute paths and fix some issues"
    This reverts commit 09aa65b185565a8abd177c595302f6cfd39d3b26.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from '@/pages/Home'
+import { Home } from './pages/Home'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(

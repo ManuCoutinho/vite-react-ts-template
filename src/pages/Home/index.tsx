6da96536890d44e8590e7043f7bc62cfd2a4eabe
Emanuela Coutinho <gdn.sasha@gmail.com>
2024-02-06 14:50:02 -0300

    refactor: update layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,6 +5,15 @@ export const Home: React.FC = () => {
       <div className='logo'>
         <img src='/logo.svg' alt='react logo' />
       </div>
+      <h3>Getting start:</h3>
+      <div className='wrapper__button'>
+        <a href='https://vitejs.dev/' rel='noopener noreferrer' target='_blank'>
+          Vite
+        </a>
+        <a href='https://react.dev/' rel='noopener noreferrer' target='_blank'>
+          React
+        </a>
+      </div>
     </section>
   )
 }
